ccb3079e02ec8326174e79a47c5355437e848ebc
Yash Raj Harsh <114059450+Yash617raj@users.noreply.github.com>
2023-01-10 22:48:41 +0530

    Add files via upload

---
+++ class208-Example2-cipherSchools.py
@@ -0,0 +1,14 @@
+class Mobile:
+    def __init__(self,name):
+        self.name = name
+        
+class MobileStore :
+    def __init__(self):
+        self.mobiles[]
+        
+    def add_mobile(self, new_mobile):
+        self.mobiles.append(new_mobile)
+        
+oneplus = Mobile("oppo k10")
+samsung = "samsung galaxy s8"                  
+    
